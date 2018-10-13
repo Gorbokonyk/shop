b252b3a9b6edd4d1343105064d9e915be9038dec
dos4dev <dos4dev@gmail.com>
2018-10-13 21:31:58 +0300

    Changed text of main page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Jerky
                 </div>
 
                 <div class="links">
